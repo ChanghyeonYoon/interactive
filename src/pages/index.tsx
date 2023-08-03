aea9cfd31a04aa9e7ee50c64fae3bdca94d6fc1d
Changhyeon Yoon <hyeon120411@gmail.com>
2023-08-03 23:39:26 +0900

    GSAP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import gsap from "gsap";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
